c081284a7ce0f1c3bc7d8d0483a6042257f227de
Николай Горичев <nikogorichev812@gmail.com>
2025-02-01 15:56:56 +0300

    FRONT-5 установка сторибука

---
+++ src/shared/config/storybook/ThemeDecorator/ThemeDecorator.tsx
@@ -0,0 +1,7 @@
+import "app/styles/index.scss";
+import { StoryFn } from "@storybook/react";
+import { Theme } from "app/providers/ThemeProvider";
+
+export const ThemeDecorator = (theme: Theme) => (Story: StoryFn) => (
+  <div className={`app ${theme}`}>{<Story />}</div>
+);
